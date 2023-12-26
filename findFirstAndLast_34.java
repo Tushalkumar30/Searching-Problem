34a7d6f4d1135252cef480608689bf853d8be3b1
Tushal Kumar <133259434+Tushalkumar30@users.noreply.github.com>
2023-12-26 22:24:57 +0530

    Update findFirstAndLast_34.java

--- findFirstAndLast_34.java
+++ findFirstAndLast_34.java
@@ -1,4 +1,42 @@
+// First solutuon
 class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        int start = search(nums, target, true);
+        int end = search(nums, target, false);
+        ans[0] = start;
+        ans[1] = end;
+        return ans;
+
+    }
+    int search( int[] nums, int target, boolean findIndex){
+        int ans = -1;
+        int start =0;
+        int end = nums.length -1;
+        while( start <= end){
+            int mid = start + ( end - start )/ 2;
+            if( target < nums[mid]){
+                end = mid -1;
+            }
+            else if( target > nums[mid]){
+                start = mid + 1;
+            }
+            else{
+                ans = mid;
+                if( findIndex){
+                    end = mid -1;
+                }
+                else{
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+
+}
+// Second Solution
+/*class Solution {
     public int[] searchRange(int[] nums, int target) {
         int start =0;
         int end = nums.length -1;
@@ -27,3 +65,4 @@ class Solution {
         return new int[] {first,last};
     }
 }
+*/
